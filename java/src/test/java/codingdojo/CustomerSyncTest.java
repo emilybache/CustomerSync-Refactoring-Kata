37fe6790e7e1d0c091a5423b9959a2ecbe1caa32
Emily Bache <emily.bache@praqma.com>
2019-07-31 15:23:34 +0200

    reset tests to use mockito

--- java/src/test/java/codingdojo/CustomerSyncTest.java
+++ java/src/test/java/codingdojo/CustomerSyncTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 
 public class CustomerSyncTest {
@@ -22,245 +23,16 @@ public class CustomerSyncTest {
         Customer customer = createCustomerWithSameCompanyAs(externalCustomer);
         customer.setExternalId(externalId);
 
-        FakeDatabase db = new FakeDatabase();
-        db.addCustomer(customer);
+        CustomerDataLayer db = mock(CustomerDataLayer.class);
+        when(db.findByExternalId(externalId)).thenReturn(customer);
         CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
 
         // ACT
         boolean created = sut.syncWithDataLayer(externalCustomer);
 
         assertFalse(created);
-        printAfterState(db, toAssert);
-        Approvals.verify(toAssert);
-    }
+        verify(db, atLeastOnce()).updateCustomerRecord(any(Customer.class));
 
-    @Test
-    public void syncPrivatePersonByExternalId(){
-        String externalId = "12345";
-
-        ExternalCustomer externalCustomer = createExternalPrivatePerson();
-        externalCustomer.setExternalId(externalId);
-
-        Customer customer = new Customer();
-        customer.setCustomerType(CustomerType.PERSON);
-        customer.setInternalId("67576");
-        customer.setExternalId(externalId);
-
-        FakeDatabase db = new FakeDatabase();
-        db.addCustomer(customer);
-        CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
-
-        // ACT
-        boolean created = sut.syncWithDataLayer(externalCustomer);
-
-        assertFalse(created);
-        printAfterState(db, toAssert);
-        Approvals.verify(toAssert);
-    }
-
-    @Test
-    public void syncShoppingLists(){
-        String externalId = "12345";
-
-        ExternalCustomer externalCustomer = createExternalCompany();
-        externalCustomer.setExternalId(externalId);
-
-        Customer customer = createCustomerWithSameCompanyAs(externalCustomer);
-        customer.setExternalId(externalId);
-        customer.setShoppingLists(Arrays.asList(new ShoppingList("eyeliner", "blusher")));
-
-        FakeDatabase db = new FakeDatabase();
-        db.addCustomer(customer);
-        CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
-
-        // ACT
-        boolean created = sut.syncWithDataLayer(externalCustomer);
-
-        assertFalse(created);
-        printAfterState(db, toAssert);
-        Approvals.verify(toAssert);
-    }
-
-    @Test
-    public void syncNewCompanyCustomer(){
-
-        ExternalCustomer externalCustomer = createExternalCompany();
-        externalCustomer.setExternalId("12345");
-
-        FakeDatabase db = new FakeDatabase();
-        CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
-
-        // ACT
-        boolean created = sut.syncWithDataLayer(externalCustomer);
-
-        assertTrue(created);
-        printAfterState(db, toAssert);
-        Approvals.verify(toAssert);
-    }
-
-    @Test
-    public void syncNewPrivateCustomer(){
-
-        ExternalCustomer externalCustomer = createExternalPrivatePerson();
-        externalCustomer.setExternalId("12345");
-
-        FakeDatabase db = new FakeDatabase();
-        CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
-
-        // ACT
-        boolean created = sut.syncWithDataLayer(externalCustomer);
-
-        assertTrue(created);
-        printAfterState(db, toAssert);
-        Approvals.verify(toAssert);
-    }
-
-    @Test
-    public void conflictExceptionWhenExistingCustomerIsPerson() {
-        String externalId = "12345";
-
-        ExternalCustomer externalCustomer = createExternalCompany();
-        externalCustomer.setExternalId(externalId);
-
-        Customer customer = new Customer();
-        customer.setCustomerType(CustomerType.PERSON);
-        customer.setInternalId("45435");
-        customer.setExternalId(externalId);
-
-        FakeDatabase db = new FakeDatabase();
-        db.addCustomer(customer);
-        CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
-
-        Assertions.assertThrows(ConflictException.class, () -> {
-            sut.syncWithDataLayer(externalCustomer);
-        }, printAfterState(db, toAssert).toString());
-
-        Approvals.verify(toAssert);
-    }
-
-    @Test
-    public void syncByExternalIdButCompanyNumbersConflict(){
-        String externalId = "12345";
-
-        ExternalCustomer externalCustomer = createExternalCompany();
-        externalCustomer.setExternalId(externalId);
-
-        Customer customer = createCustomerWithSameCompanyAs(externalCustomer);
-        customer.setExternalId(externalId);
-        customer.setCompanyNumber("000-3234");
-
-        FakeDatabase db = new FakeDatabase();
-        db.addCustomer(customer);
-        CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
-
-        // ACT
-        boolean created = sut.syncWithDataLayer(externalCustomer);
-
-        assertTrue(created);
-        printAfterState(db, toAssert);
-        Approvals.verify(toAssert);
-    }
-
-
-    @Test
-    public void syncByCompanyNumber(){
-        String companyNumber = "12345";
-
-        ExternalCustomer externalCustomer = createExternalCompany();
-        externalCustomer.setCompanyNumber(companyNumber);
-
-        Customer customer = createCustomerWithSameCompanyAs(externalCustomer);
-        customer.setCompanyNumber(companyNumber);
-        customer.addShoppingList(new ShoppingList("eyeliner", "mascara", "blue bombe eyeshadow"));
-
-        FakeDatabase db = new FakeDatabase();
-        db.addCustomer(customer);
-        CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
-
-        // ACT
-        boolean created = sut.syncWithDataLayer(externalCustomer);
-
-        assertFalse(created);
-        printAfterState(db, toAssert);
-        Approvals.verify(toAssert);
-    }
-
-    @Test
-    public void syncByCompanyNumberWithConflictingExternalId(){
-        String companyNumber = "12345";
-
-        ExternalCustomer externalCustomer = createExternalCompany();
-        externalCustomer.setCompanyNumber(companyNumber);
-        externalCustomer.setExternalId("45646");
-
-        Customer customer = createCustomerWithSameCompanyAs(externalCustomer);
-        customer.setCompanyNumber(companyNumber);
-        customer.setExternalId("conflicting id");
-
-        FakeDatabase db = new FakeDatabase();
-        db.addCustomer(customer);
-        CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
-
-        // ACT
-        Assertions.assertThrows(ConflictException.class, () -> {
-            sut.syncWithDataLayer(externalCustomer);
-        }, printAfterState(db, toAssert).toString());
-
-        Approvals.verify(toAssert);
-    }
-
-    @Test
-    public void conflictExceptionWhenExistingCustomerIsCompany() {
-        String externalId = "12345";
-
-        ExternalCustomer externalCustomer = createExternalPrivatePerson();
-        externalCustomer.setExternalId(externalId);
-
-        Customer customer = new Customer();
-        customer.setCustomerType(CustomerType.COMPANY);
-        customer.setCompanyNumber("32423-342");
-        customer.setInternalId("45435");
-        customer.setExternalId(externalId);
-
-        FakeDatabase db = new FakeDatabase();
-        db.addCustomer(customer);
-        CustomerSync sut = new CustomerSync(db);
-
-        StringBuilder toAssert = printBeforeState(externalCustomer, db);
-
-        Assertions.assertThrows(ConflictException.class, () -> {
-            sut.syncWithDataLayer(externalCustomer);
-        }, printAfterState(db, toAssert).toString());
-
-        Approvals.verify(toAssert);
-    }
-
-    private ExternalCustomer createExternalPrivatePerson() {
-        ExternalCustomer externalCustomer = new ExternalCustomer();
-        externalCustomer.setExternalId("12345");
-        externalCustomer.setName("Joe Bloggs");
-        externalCustomer.setAddress(new Address("123 main st", "Stockholm", "SE-123 45"));
-        externalCustomer.setPreferredStore("Nordstan");
-        externalCustomer.setShoppingLists(Arrays.asList(new ShoppingList("lipstick", "foundation")));
-        return externalCustomer;
     }
 
 
@@ -282,19 +54,4 @@ public class CustomerSyncTest {
         return customer;
     }
 
-    private StringBuilder printBeforeState(ExternalCustomer externalCustomer, FakeDatabase db) {
-        StringBuilder toAssert = new StringBuilder();
-        toAssert.append("BEFORE:\n");
-        toAssert.append(db.printContents());
-
-        toAssert.append("\nSYNCING THIS:\n");
-        toAssert.append(ExternalCustomerPrinter.print(externalCustomer, ""));
-        return toAssert;
-    }
-
-    private StringBuilder printAfterState(FakeDatabase db, StringBuilder toAssert) {
-        toAssert.append("\nAFTER:\n");
-        toAssert.append(db.printContents());
-        return toAssert;
-    }
 }
